72e78c40948ade022ac9300ae62317cfaf68b2ff
Gestión de la Educación Autónoma (GEA) <barraorion@gmail.com>
2011-07-09 17:11:15 +0200

    first version scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tew::Application.config.secret_token = '74cb9c73d591ff88c423e4f19379d82a6557aebde08db5eb1e4a2532367f893d99e9cdda6dfe9b70f7c0101bcfcc17a357f54bbfc81cff3659fe140e5dc2ea69'
