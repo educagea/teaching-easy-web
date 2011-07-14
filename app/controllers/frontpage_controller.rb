fd334492a8b02785ebcf6daae5068cba22ecfa61
educagea <educagea@pizarro.(none)>
2011-07-14 18:43:10 +0200

    second layout version

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -1,11 +1,6 @@
 class FrontpageController < ApplicationController
   
-  def index
-    respond_to do |format|
-      format.html # index.html.erb
-    end
-  end
-
+  
   # Webfinger protocol
   # http://code.google.com/p/webfinger/
   def host_meta
